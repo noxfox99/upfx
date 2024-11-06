c4f70c0b9723ab1f16ddc749ed222219586258ef
Nox Fox <rickxxxmorty@proton.me>
2024-11-06 12:30:12 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -124,14 +124,39 @@ const UploadService = () => {
           Перетащите файлы или кликните для загрузки
         </label>
       </div>
-            <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-    <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-    <label for="bordered-checkbox-1" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Сжать изображение перед загрузкой</label>
+           <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+  <input
+    id="bordered-checkbox-1"
+    type="checkbox"
+    value=""
+    name="bordered-checkbox"
+    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+  />
+  <label
+    htmlFor="bordered-checkbox-1"
+    className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+  >
+    Сжать изображение перед загрузкой
+  </label>
 </div>
-<div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-    <input checked id="bordered-checkbox-2" type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-    <label for="bordered-checkbox-2" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Шифровать файлы перед загрузкой</label>
+
+<div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+  <input
+    id="bordered-checkbox-2"
+    type="checkbox"
+    value=""
+    name="bordered-checkbox"
+    defaultChecked
+    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+  />
+  <label
+    htmlFor="bordered-checkbox-2"
+    className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+  >
+    Шифровать файлы перед загрузкой
+  </label>
 </div>
+
       {files.length > 0 && (
         <div className="mt-6 w-full max-w-lg">
           <h2 className="text-2xl font-bold mb-4">Ваши Файлы:</h2>
