9fb52db4c8fc3beebcc5d7feaf58dde829358146
Nox Fox <rickxxxmorty@proton.me>
2024-11-06 14:42:31 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -185,6 +185,11 @@ const UploadService = () => {
             required
           ></textarea>
         </div>
+             <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+           <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+               Post comment
+           </button>
+                  </div>  
       </div>
         </div>
       )}
