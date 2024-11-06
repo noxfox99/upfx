51c89a72c69d1522df0f15703a48f37b54e85f6e
Nox Fox <rickxxxmorty@proton.me>
2024-11-06 12:43:55 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -124,9 +124,7 @@ const UploadService = () => {
           Перетащите файлы или кликните для загрузки
         </label>
       </div>
-          <div>
-      <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">Identification</h3>
-      <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
           <div className="flex items-center ps-3">
             <input
@@ -139,7 +137,7 @@ const UploadService = () => {
               htmlFor="vue-checkbox-list"
               className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              Vue JS
+              Продвинутое шифрование
             </label>
           </div>
         </li>
@@ -155,12 +153,12 @@ const UploadService = () => {
               htmlFor="react-checkbox-list"
               className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              React
+              Сжать и удалить Metadata
             </label>
           </div>
         </li>
       </ul>
-    </div>  
+     
 
       {files.length > 0 && (
         <div className="mt-6 w-full max-w-lg">
