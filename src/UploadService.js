c2b69ad5d641ffd05ae5a1ce78984fad4ca2c7ef
Nox Fox <rickxxxmorty@proton.me>
2024-11-19 19:56:36 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { PinataSDK } from "pinata";
 import QRCode from 'qrcode.react'; // Импортируем компонент QRCode
