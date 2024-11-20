ad6b10053086121fe1ba75881206550015423b82
Nox Fox <rickxxxmorty@proton.me>
2024-11-20 19:41:36 +0300

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -9,15 +9,17 @@ app.use(cors());
 
 app.get('/proxy', async (req, res) => {
   const targetUrl = req.query.url;
-
+  console.log('start proxy');
+  console.log(targetUrl);
   if (!targetUrl) {
     return res.status(400).json({ error: 'URL is required' });
   }
 
   try {
+    console.log('x1');
     const response = await fetch(targetUrl);
     const data = await response.text();
-
+    console.log(data);
     res.set('Content-Type', response.headers.get('Content-Type'));
     res.send(data);
   } catch (error) {
