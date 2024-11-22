5294458937769982737096080289c9af1f476fa3
Nox Fox <rickxxxmorty@proton.me>
2024-11-22 15:11:42 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -216,7 +216,7 @@ const handleUpload = async () => {
         </div>
         </div>
       </header>
-      <h1 className="text-white text-2xl sm:text-2xl py-2 text-gradient ">Загрузите файлы в IPFS</h1>
+      {!filesUploaded && ( <h1 className="text-white text-2xl sm:text-2xl py-2 text-gradient ">Загрузите файлы в IPFS</h1>  )}
                   {!filesUploaded && (
       <div className="border-dashed border-4 border-white p-10 w-full max-w-lg text-center cursor-pointer bg-gray-900 rounded-lg shadow-lg hover:opacity-75 transition"
         onDrop={handleDrop}
