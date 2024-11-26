5bdf0ceb6f6f0c107b0f404cf5021fdccce6699c
Nox Fox <rickxxxmorty@proton.me>
2024-11-26 18:13:46 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -361,8 +361,8 @@ style={{
           {/* Error Icon (SVG) */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="16"
+            height="16"
             viewBox="0 0 32 32"
             style={{ marginRight: "8px" }}
           >
