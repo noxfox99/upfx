855eff7ca0080f53d83e0617b6cb6b486fce4fa0
Nox Fox <rickxxxmorty@proton.me>
2024-11-27 16:08:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UploadService from './UploadService';
 import GalleryPage from './GalleryPage';
+import Doc from './Doc';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UploadService />} />
         <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/doc" element={<Doc />} />
       </Routes>
     </Router>
   );
